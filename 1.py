e367131c261aa190bebfdf9db395e83521f304e6
Sempai108 <genshimskam@gmail.com>
2024-12-19 19:04:12 +0300

    =

--- 1.py
+++ 1.py
@@ -7,23 +7,26 @@ servo_pin = 18
 GPIO.setup(servo_pin, GPIO.OUT)
 
 # Создаем объект PWM
-pwm = GPIO.PWM(servo_pin, 50)  # 50 Гц (период 20 мс)
+pwm = GPIO.PWM(servo_pin, 50)  # Частота 50 Гц
 pwm.start(0)
 
 def set_angle(angle):
     duty = angle / 18 + 2
-    GPIO.output(servo_pin, True)
     pwm.ChangeDutyCycle(duty)
     time.sleep(1)
-    GPIO.output(servo_pin, False)
-    pwm.ChangeDutyCycle(2)
+    pwm.ChangeDutyCycle(0)
 
 try:
     while True:
-        set_angle(90)  # Поворот на 90 градусов
-        time.sleep(2)
-        set_angle(45)   # Поворот на 0 градусов
-        time.sleep(2)
+        set_angle(0)    # Устанавливаем угол поворота на 0 градусов
+        time.sleep(1)   # Ждем 1 секунду
+
+        set_angle(90)   # Устанавливаем угол поворота на 90 градусов
+        time.sleep(1)   # Ждем 1 секунду
+
+        set_angle(180)  # Устанавливаем угол поворота на 180 градусов
+        time.sleep(1)   # Ждем 1 секунду
 finally:
     pwm.stop()
     GPIO.cleanup()
+d
