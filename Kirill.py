24340bccd12f7122d903635df6bb72b616957a28
Sempai108 <genshimskam@gmail.com>
2024-12-28 17:38:57 +0300

    +

--- Kirill.py
+++ Kirill.py
@@ -1,37 +1,26 @@
-import cv2
-import time
-from PIL import Image, ImageChops
+import cv2  # Импортируем библиотеку OpenCV для работы с видео и изображениями
+from PIL import Image, ImageChops  # Импортируем классы Image и ImageChops из библиотеки Pillow для работы с изображениями
+import time  # Импортируем библиотеку для работы с временем
 
+camera = cv2.VideoCapture(0)  # Инициализируем захват видео с камеры (номер 0)
+count = 0  # Инициализируем счетчик пикселей)
 
-time.sleep(1)
-camera = cv2.VideoCapture(0)
-time.sleep(1)
-count = 0
 def yes_or_not():
-    if count <= 30000:
-        return 0
-    else:
-        return 1
+    global count
+    return 1 if count > 30000 else 0
 
-
-
-def is_pixel_black_or_write(pixel):
+def is_pixel_black_or_white(pixel):
     red, green, blue = pixel
     average = (red + green + blue) / 3
-    if average >= 20:
-        return 1
-    else:
-        return 0
+    return 1 if average >= 30 else 0
 
 def difference():
     old = 0
-    human = 0
     global count
     while True:
         time.sleep(3)
         good, img = camera.read()
         cv2.imwrite('w1.png', img)
-        cv2.imshow("Image", img)
         image_1 = Image.open("w.png")
         image_2 = Image.open("w1.png")
 
@@ -39,30 +28,33 @@ def difference():
         result.save('result.jpg')
         count = 0
         res = cv2.imread('result.jpg', cv2.IMREAD_GRAYSCALE)
-        cv2.imshow('RES', res)
         image = Image.open('result.jpg')
         width, height = image.size
         for y in range(height):
             for x in range(width):
                 pixel = image.getpixel((x, y))
-                color = is_pixel_black_or_write(pixel)
-                count = count + color
+                color = is_pixel_black_or_white(pixel)
+                count += color
         print(count, width * height)
-
         human = yes_or_not()
 
-
         if old == 1 and human == 1:
-
-            print("HUMAN")
-            old = 0
+            print("PERSON WAS DISCOVERED")
             human = 0
         else:
             old = human
-while True:
-    good, img = camera.read()
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) == ord('r'):
-        good, image = camera.read()
-        cv2.imwrite("w.png", image)
-        difference()
+
+        # Отображаем только начальное изображение
+        cv2.imshow("Initial Image", cv2.imread('w.png'))
+
+        # Проверка нажатия клавиши 'q' для выхода
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# Основная часть программы
+difference()
+
+camera.release()
+cv2.destroyAllWindows()
+GPIO.cleanup()
+ц
