561984abfd8f1a77a87060fdc89b0b2887f6c385
Sempai108 <genshimskam@gmail.com>
2024-12-28 17:42:52 +0300

    +

--- 3.py
+++ 3.py
@@ -1,8 +1,8 @@
 import cv2  # Импортируем библиотеку OpenCV для работы с видео и изображениями
-from PIL import Image, \
-    ImageChops  # Импортируем классы Image и ImageChops из библиотеки Pillow для работы с изображениями
+from PIL import Image, ImageChops  # Импортируем классы Image и ImageChops из библиотеки Pillow для работы с изображениями
 import RPi.GPIO as GPIO  # Импортируем библиотеку для работы с GPIO
 import time  # Импортируем библиотеку для работы с временем
+import os  # Импортируем библиотеку для работы с файловой системой
 
 camera = cv2.VideoCapture(0)  # Инициализируем захват видео с камеры (номер 0)
 count = 0  # Инициализируем счетчик пикселей
@@ -25,13 +25,11 @@ pwm1.start(0)
 pwm2 = GPIO.PWM(servo_pin2, 50)  # Частота 50 Гц
 pwm2.start(0)
 
-
 def set_angle1(angle1):
     duty1 = angle1 / 18 + 2
     pwm1.ChangeDutyCycle(duty1)
     time.sleep(1)
     pwm1.ChangeDutyCycle(0)
-
 
 def set_angle2(angle2):
     duty2 = angle2 / 18 + 2
@@ -39,51 +37,41 @@ def set_angle2(angle2):
     time.sleep(1)
     pwm2.ChangeDutyCycle(0)
 
-
 def yes_or_not():
-    if count <= 30000:
-        value = 0
-        return value
-    else:
-        value = 1
-        return value
+    global count
+    return 1 if count > 30000 else 0
 
-
-def is_pixel_black_or_write(pixel):
+def is_pixel_black_or_white(pixel):
     red, green, blue = pixel
     average = (red + green + blue) / 3
-    if average >= 30:
-        return 1
-    else:
-        return 0
-
+    return 1 if average >= 30 else 0
 
 def difference():
     old = 0
     global count
+
     while True:
         time.sleep(3)
         good, img = camera.read()
         cv2.imwrite('w1.png', img)
-        image_1 = Image.open("../w.png")
-        image_2 = Image.open("../w1.png")
+        image_1 = Image.open("w.png")
+        image_2 = Image.open("w1.png")
 
         result = ImageChops.difference(image_1, image_2)
         result.save('result.jpg')
         count = 0
         res = cv2.imread('result.jpg', cv2.IMREAD_GRAYSCALE)
-        image = Image.open('../result.jpg')
+        image = Image.open('result.jpg')
         width, height = image.size
         for y in range(height):
             for x in range(width):
                 pixel = image.getpixel((x, y))
-                color = is_pixel_black_or_write(pixel)
-                count = count + color
+                color = is_pixel_black_or_white(pixel)
+                count += color
         print(count, width * height)
         human = yes_or_not()
 
         if old == 1 and human == 1:
-
             print("PERSON WAS DISCOVERED")
             set_angle1(0)  # Устанавливаем угол поворота на 0 градусов
             set_angle1(90)  # Устанавливаем угол поворота на 90 градусов
@@ -106,3 +94,4 @@ difference()
 
 camera.release()
 cv2.destroyAllWindows()
+GPIO.cleanup()
