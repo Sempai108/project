ff3f6a4c4b1c9cf3919c89193439425a8a50ecd7
Sempai108 <genshimskam@gmail.com>
2025-02-03 15:38:57 +0300

    +

--- test_TTS.py
+++ test_TTS.py
@@ -6,7 +6,7 @@ pygame.mixer.init()
 # Загрузка и воспроизведение MP3-файла
 pygame.mixer.music.load("C:\\Users\\1\\Documents\\ПР\\TTS\\TTS_1.mp3")
 pygame.mixer.music.play()
-с
+
 # Задержка для воспроизведения аудио до конца
 while pygame.mixer.music.get_busy():
     pygame.time.Clock().tick(10)
