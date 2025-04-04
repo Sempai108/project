2c7d249c75144afc81d3ee6143308c178128a4c9
Sempai108 <genshimskam@gmail.com>
2025-04-04 06:40:10 +0300

    +

--- oled1.py
+++ oled1.py
@@ -1,14 +1,27 @@
-from Adafruit_SSD1306 import SSD1306_128x64
+import board
+import busio
+from adafruit_ssd1306 import SSD1306_I2C
 from PIL import Image, ImageDraw
 
-display = SSD1306_128x64()
-display.begin()
-display.clear()
-display.display()
+# Настройка I2C
+i2c = busio.I2C(board.SCL, board.SDA)
 
-image = Image.new('1', (128, 64))
+# Инициализация дисплея
+WIDTH = 128
+HEIGHT = 64
+display = SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
+
+# Очистка дисплея
+display.fill(0)
+display.show()
+
+# Создание изображения
+image = Image.new("1", (WIDTH, HEIGHT))
 draw = ImageDraw.Draw(image)
-draw.text((0, 0), 'Hello, OLED!', fill=255)
-display.image(image)
-display.display()
 
+# Рисуем текст
+draw.text((0, 0), "Hello, OLED!", fill=255)
+
+# Отображаем на экране
+display.image(image)
+display.show()
